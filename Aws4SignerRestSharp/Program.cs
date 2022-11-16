5e121c97c43750084a87608dad23964a7aa57b25
Sooraj Kumar <sooraj.kumar@astera.com>
2022-11-16 16:23:23 +0500

    tester code

--- Aws4SignerRestSharp/Program.cs
+++ Aws4SignerRestSharp/Program.cs
@@ -1,16 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RestSharp;
 using Aws4SignerRestSharp;
-Console.WriteLine("Hello, World!");
-NewCall();
-Console.ReadLine();
-void NewCall()
+
+Test();
+
+void Test()
 {
-    var client = new RestClient("https://alibhutto.s3.us-east-1.amazonaws.com");
-    var req = new RestRequest("?acl=alibhutto", Method.Get);
-    Uri u = new("https://alibhutto.s3.us-east-1.amazonaws.com/?acl=alibhutto");
-    Aws4SignContext context = new(u, "us-east-1", "s3", "AKIAWUY7ISKCS6GL5UAE", "E+GX2HhiPJzeQu3vAseZ7gpnmBdq8JHQMaenSq55");
-    var nr = new Aws4Signer(context);
-    nr.Sign(req);
-    var res = client.Execute(req);
+    /*
+     Just set these fields and will work fine.
+     */
+    var client = new RestClient("Your_BaseUrl");
+    var request = new RestRequest("Your_Resource", Method.Get);
+    Uri completeUrl = new("BaseUrl/Resource?QueryString");
+    Aws4SignContext context = new(completeUrl, "Region", "Service", "Access_Key", "Secret_Key");
+    var signer = new Aws4Signer(context);
+    signer.Sign(request);
+    var response = client.Execute(request);
 }
